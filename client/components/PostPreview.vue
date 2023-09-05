aa17aca00bf7cae7fb94a88532845e4bd8921a6a
Wesley Alves Rolim <walvesrolim@gmail.com>
2023-09-05 00:47:39 -0300

    feat: add PostPreview component and home page

---
+++ client/components/PostPreview.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import type { PostAttributes } from "~/domain/protocols";
+
+defineProps({
+  post: Object as PropType<PostAttributes>,
+});
+</script>
+
+<template>
+  <div
+    class="max-w-[200px] md:max-w-[400px] rounded-lg overflow-hidden"
+    v-if="post?.title"
+  >
+    <NuxtLink class="text-white hover:opacity-100" :to="`/post/${post.slug}`">
+      <div class="overflow-hidden md:h-[350px]">
+        <img
+          class="w-full min-h-full hover:scale-125 hover:rotate-6"
+          :src="post.cover.data.attributes.formats.medium?.url"
+          :alt="post.cover.data.attributes.alternativeText"
+        />
+      </div>
+      <h1 class="text-2xl my-2 hover:text-emerald-200">{{ post.title }}</h1>
+      <p class="text-sm h-10 line-clamp-2 overflow-ellipsis hover:underline">
+        {{ post.content }}
+      </p>
+    </NuxtLink>
+  </div>
+</template>
+
+<style scoped>
+div * {
+  @apply transition-all duration-300 ease-in-out;
+}
+</style>
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,6 +1,21 @@
+<script setup lang="ts">
+import { getAllPostsUseCase } from "~/data/posts";
+
+const data = await getAllPostsUseCase(
+  "?populate=*&sort=id:desc&pagination[pageSize]=10"
+);
+</script>
+
 <template>
-  <div class="w-full h-full flex flex-col justify-center items-center gap-4">
-    <h1>Blog</h1>
-    <Button> Some button </Button>
-  </div>
+  <section class="w-11/12 max-w-screen-lg mx-auto grid gap-4">
+    <h1 class="text-4xl">All Posts</h1>
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-8">
+      <PostPreview
+        v-if="data?.posts"
+        v-for="post in data.posts"
+        :key="post.slug"
+        :post="post"
+      />
+    </div>
+  </section>
 </template>
