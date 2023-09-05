3e8abfe1bca013c6ae9da09c05b2bf4f0d77cb91
Wesley Alves Rolim <walvesrolim@gmail.com>
2023-09-05 00:42:41 -0300

    feat: add post protocol

---
+++ client/domain/protocols/post.ts
@@ -0,0 +1,77 @@
+export interface Root {
+  data: Daum[];
+  meta: Meta;
+}
+
+export interface Daum {
+  id: number;
+  attributes: PostAttributes;
+}
+
+export interface PostAttributes {
+  title: string;
+  content: string;
+  slug: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  cover: Cover;
+}
+
+export interface Cover {
+  data: Data;
+}
+
+export interface Data {
+  id: number;
+  attributes: Attributes2;
+}
+
+export interface Attributes2 {
+  name: string;
+  alternativeText: string;
+  caption: any;
+  width: number;
+  height: number;
+  formats: Formats;
+  hash: string;
+  ext: string;
+  mime: string;
+  size: number;
+  url: string;
+  previewUrl: any;
+  provider: string;
+  provider_metadata: any;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface Formats {
+  large?: CoverFormat;
+  small?: CoverFormat;
+  medium?: CoverFormat;
+  thumbnail?: CoverFormat;
+}
+
+export interface CoverFormat {
+  ext: string;
+  url: string;
+  hash: string;
+  mime: string;
+  name: string;
+  path: any;
+  size: number;
+  width: number;
+  height: number;
+}
+
+export interface Meta {
+  pagination: Pagination;
+}
+
+export interface Pagination {
+  page: number;
+  pageSize: number;
+  pageCount: number;
+  total: number;
+}
