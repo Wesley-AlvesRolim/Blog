301b3e338255fcfbbf1b3d479d39b4db7fafe5b2
Wesley Alves Rolim <walvesrolim@gmail.com>
2023-09-06 11:14:07 -0300

    feat: add Comment component

---
+++ client/components/Comments.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+defineProps({
+  pageId: {
+    type: String,
+    required: true,
+  },
+});
+
+onMounted(() => {
+  const scriptLoader = document.createElement("script");
+  scriptLoader.type = "module";
+  scriptLoader.async = true;
+  scriptLoader.src = "https://talk.hyvor.com/embed/embed.js";
+  document.querySelector("#comments")?.appendChild(scriptLoader);
+});
+</script>
+
+<template>
+  <section id="comments">
+    <hyvor-talk-comments
+      website-id="9473"
+      :page-id="pageId"
+    ></hyvor-talk-comments>
+  </section>
+</template>
